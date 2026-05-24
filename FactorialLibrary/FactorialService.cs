9f940da1c064d45a748d382182929b3336465b8d
thejpala <pala.jagadeesh@gmail.com>
2026-05-24 17:33:25 +0000

    Factorial functionality added

---
+++ FactorialLibrary/FactorialService.cs
@@ -0,0 +1,13 @@
+﻿namespace FactorialLibrary;
+
+public class FactorialService{
+    public static int Factorial(int n) {
+        if (n < 0){
+            throw new ArgumentOutOfRangeException(nameof(n), "Factorial is undefined for negative numbers.");
+        }else if(n < 2){
+             return 1;
+        }
+        return n * Factorial(n-1);
+
+  }
+}
