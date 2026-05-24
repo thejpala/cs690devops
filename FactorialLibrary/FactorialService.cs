3c409797d817cd6938ee47ecf28f2aaa284236e0
thejpala <pala.jagadeesh@gmail.com>
2026-05-24 17:44:28 +0000

    Adding negative number validations

--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -3,7 +3,7 @@
 public class FactorialService{
     public static int Factorial(int n) {
         if (n < 0){
-            throw new ArgumentOutOfRangeException(nameof(n), "Factorial is undefined for negative numbers.");
+            throw new InvalidOperationException("Negative numbers don't have proper factorial");
         }else if(n < 2){
              return 1;
         }
